6593b960064cf6cf3ef13e49b215d77817abf3b4
Daniel <danielkaiss@hotmail.com>
2018-05-18 00:12:19 -0300

    Criando Controllers

--- AlienCity3D_Fase5/Assets/Scripts/PlayerController.cs
+++ AlienCity3D_Fase5/Assets/Scripts/PlayerController.cs
@@ -25,39 +25,48 @@ public class PlayerController : MonoBehaviour {
 	{
 		Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
 		transform.Rotate (new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
-		
+		//se o personagem não estiver tocando o solo
 		if (!cc.isGrounded) {
 			gravidade += Physics.gravity * Time.deltaTime;
 		} 
 		else 
 		{
+			//se o personagem estiver sobre alguma superficie, a gravidade e e zerada
 			gravidade = Vector3.zero;
 			if(jump)
 			{
+				//se o pers esta pulando a gravidade assume o valor de 6f
 				gravidade.y = 6f;
+				//desliga o pulo para poder pular de novo quando tocar o solo
 				jump = false;
 			}
 		}
 		move += gravidade;
 		cc.Move (move* Time.deltaTime);
+		//metodo anima é responsavel pelo controle das animações
 		Anima ();
 	}
 	 
 	void Anima()
 	{
+		//se nenhuma tecla esta sendo precionada
 		if (!Input.anyKey)
 		{
+			//chmar a animação de parado.
 			anim.SetTrigger("Parado");
 		} 
 		else 
 		{
+			//senao verifica se esta sendo precionado a tecla espaço
 			if(Input.GetKeyDown("space"))
 			{
+				//chamar a animação do pulo
 				anim.SetTrigger("Pula");
 				jump = true;
 			}
 			else
 			{
+				//caso contrario chamar a animação da corrida
 				anim.SetTrigger("Corre");
 			}
 		}
